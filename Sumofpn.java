778fe9732bc9cf97295de07aa43026463f9585b9
SanthoshKumar-Selvaraj <41115438+SanthoshKumar-Selvaraj@users.noreply.github.com>
2018-07-20 11:18:20 +0530

    Add files via upload

---
+++ Sumofpn.java
@@ -0,0 +1,22 @@
+package guvi1;
+
+import java.util.Scanner;
+
+public class Sumofpn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		int sum = 0;
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextInt();
+		int b[] = new int[a];
+		for(int i = 0 ; i < a ; i++){
+			b[i] = sc.nextInt();
+		}
+		for(int i = 0 ; i < a ; i++){
+			sum = sum + b[i];
+		}
+		System.out.println(sum);
+	}
+}
